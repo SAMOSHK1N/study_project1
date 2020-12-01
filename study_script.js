bd257bc62e3b1a2b7ac1044a5a4a62354bc8b685
Dmitrii Samoshkin <de.metro.samuel@gmail.com>
2020-12-01 22:09:56 +0300

    выполнено первое задание

--- study_script.js
+++ study_script.js
@@ -23,4 +23,17 @@
 
 'use strict';
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "")
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+const rating = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.lastFilm = rating;
